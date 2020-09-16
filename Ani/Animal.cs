bb437b18ca3bcf159dfe36108baec2a51963b151
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-16 11:26:02 +0700

    Add project files.

---
+++ Ani/Animal.cs
@@ -0,0 +1,38 @@
+﻿using Ani;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Ani
+{
+    public abstract class Animal
+    {
+        protected int Weight { get; set; }
+        protected int Height { get; set; }
+
+        public Animal(int weight, int height)
+        {
+            this.Weight = weight;
+            this.Height = height;
+        }
+        public abstract void PrintInfo();
+    }
+
+
+
+    class Cat : Animal
+    {
+        private string Name { get; set; }
+        public Cat(int weight, int height, string name) : base(weight, height)
+        {
+            this.Name = name;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Weight:{0}" +" va "+ "Height:{1}" + " va " + "Name:{2}",this.Height,this.Weight,this.Name);
+           
+        }
+    } 
+}
+
---
+++ Ani/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat ca = new Cat(12, 20, "meo");
+            ca.PrintInfo();
+            Console.ReadLine();
+        }
+    }
+}
